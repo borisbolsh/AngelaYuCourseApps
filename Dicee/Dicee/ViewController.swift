ba562e35cc61d7ebfa64b19443d9429aec90b613
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-07-21 21:52:01 +0300

    Last change to dice randomization

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -12,19 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
-    
-    var leftSideNumber = 1
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
 
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageViewOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftSideNumber]
         
-        leftSideNumber += 1
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+
     }
     
 }
