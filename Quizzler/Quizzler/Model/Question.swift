9d3a9aff67908dbfa8018fc7adce563cf5bee6d7
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-07-25 23:14:21 +0300

    Quizzler app added

---
+++ Quizzler/Quizzler/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
